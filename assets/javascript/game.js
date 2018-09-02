34cb3431d6b0b8a03345c60853b0d5761a19c8e5
Short Stack <41342213+rchlblns@users.noreply.github.com>
2018-09-01 22:03:25 -0500

    gems are clickable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
 
+$("#pink-gem").on("click", function() {
+    console.log("You clicked the pink gem!");
+});
+
+$("#purple-gem").on("click", function() {
+    console.log("You clicked the purple gem!");
+});
+
+$("#teal-gem").on("click", function() {
+    console.log("You clicked the teal gem!");
+});
+
+$("#yellow-gem").on("click", function() {
+    console.log("You clicked the yellow gem!");
+});
+
+var targetNumber = Math.floor(Math.random() * 200);
+
+$("#number-to-guess").text(targetNumber);
+    console.log(targetNumber);
 
 });
